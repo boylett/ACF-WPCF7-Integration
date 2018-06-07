6f1d8f89f9a02d044489d77fefa6ba18cdb92801
Boylett <boylett@users.noreply.github.com>
2018-06-07 15:45:00 +0100

    Create acf-wpcf7-integration.php

---
+++ acf-wpcf7-integration.php
@@ -0,0 +1,112 @@
+<?php
+
+	/**
+	 * Plugin Name:  ACF WPCF7 Integration
+	 * Plugin URI:   https://github.com/boylett/ACF-WPCF7-Integration
+	 * Description:  Integrate ACF with WPCF7 to enable custom fields on a contact form
+	 * Version:      0.0.1
+	 * Author:       Ryan Boylett
+	 * Author URI:   http://github.com/boylett/
+	 */
+	
+	add_action('save_post', function($post_id)
+	{
+		if(function_exists('wpcf7_current_action') and wpcf7_current_action() == 'save')
+		{
+			$post = get_post($post_id);
+
+			if($post->post_type == 'wpcf7_contact_form')
+			{
+				acf_save_post($post->ID);
+			}
+		}
+	});
+	
+	add_filter('wpcf7_editor_panels', function($panels)
+	{
+		if(isset($_GET['post']))
+		{
+			$panels['custom-fields-panel'] = array
+			(
+				"title"    => "Custom Fields",
+				"callback" => function()
+				{
+					$GLOBALS['wpcf7form-display'] = true;
+
+					$groups    = acf_get_field_groups($_GET['post']);
+					$group_ids = array();
+
+					foreach($groups as $group)
+					{
+						$group_ids[] = $group['key'];
+					}
+
+					acf_form(array
+					(
+						"form"               => false,
+						"id"                 => "wpcf7-form",
+						"post_id"            => $_GET['post'],
+						"field_groups"       => $group_ids,
+						"html_submit_button" => '<input type="submit" style="display: none;" />'
+					));
+				}
+			);
+
+			$settings = $panels['additional-settings-panel'];
+
+			unset($panels['additional-settings-panel']);
+
+			$panels['additional-settings-panel'] = $settings;
+		}
+
+		return $panels;
+	});
+
+	add_filter('acf/location/rule_types', function($choices)
+	{
+		$choices['Forms']['wpcf7'] = 'Contact Form';
+
+		return $choices;
+	});
+
+	add_filter('acf/location/rule_values/wpcf7', function($choices)
+	{
+		$forms = get_posts(array
+		(
+			"order"          => "ASC",
+			"orderby"        => "post_title",
+			"posts_per_page" => -1,
+			"post_type"      => "wpcf7_contact_form"
+		));
+
+		$choices['all'] = 'Any';
+
+		if(!empty($forms))
+		{
+			foreach($forms as $post)
+			{
+				$choices[$post->ID] = $post->post_title;
+			}
+		}
+
+		return $choices;
+	});
+
+	add_filter('acf/location/rule_match/wpcf7', function($match, $rule, $options)
+	{
+		if(isset($GLOBALS['wpcf7form-display']) and $GLOBALS['wpcf7form-display'])
+		{
+			$post_id = (int) $rule['value'];
+
+			if($rule['operator'] == "==")
+			{
+				return ($post_id == 'all' or $_GET['post'] == $post_id);
+			}
+			elseif($rule['operator'] == "!=")
+			{
+				return ($post_id != 'all' and $_GET['post'] != $post_id);
+			}
+		}
+
+		return false;
+	}, 10, 3);
